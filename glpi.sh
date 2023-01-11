86349e3669dbcbd238ad69a32fc7e394f41c39b8
Maxwell Diniz <max220893@gmail.com>
2023-01-11 17:57:18 -0300

    add crontab script.sh

--- glpi.sh
+++ glpi.sh
@@ -4,8 +4,12 @@ FILE=~/glpi/glpi-agent_1.4-1_all.deb
 if test -f "$FILE"; then
     echo -e "\n$FILE exists.\n"
 else
-    cd ~/glpi && wget https://github.com/glpi-project/glpi-agent/releases/download/1.4/glpi-agent_1.4-1_all.deb
+    cd /root/glpi && wget https://github.com/glpi-project/glpi-agent/releases/download/1.4/glpi-agent_1.4-1_all.deb
 fi
-chmod 777 ~/glpi/glpi-agent_1.4-1_all.deb
-sudo apt install -y ~/glpi/glpi-agent_1.4-1_all.deb
+chmod 777 /root/glpi/glpi-agent_1.4-1_all.deb
+sudo apt install -y /root/glpi/glpi-agent_1.4-1_all.deb
 sudo glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon
+
+command="glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon"
+job="0 15 * * * $command"
+cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | sudo crontab - -u root
