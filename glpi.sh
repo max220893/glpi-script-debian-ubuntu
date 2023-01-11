581e1267d4b0bffcc0d575cd269589b6490a838d
Maxwell Diniz <max220893@gmail.com>
2023-01-11 18:01:28 -0300

    update glpi.sh

--- glpi.sh
+++ glpi.sh
@@ -6,6 +6,8 @@ else
     cd /root/glpi && wget https://github.com/glpi-project/glpi-agent/releases/download/1.4/glpi-agent_1.4-1_all.deb
 fi
 chmod 777 /root/glpi/glpi-agent_1.4-1_all.deb
+apt update -y
+clear
 apt install -y /root/glpi/glpi-agent_1.4-1_all.deb
 glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon
 
