001276b8dd0bf91efc766c0999f01311c03b028f
Maxwell Diniz <max220893@gmail.com>
2023-01-11 17:59:37 -0300

    [master] update glpi.sh

--- glpi.sh
+++ glpi.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir -p ~/glpi
 FILE=~/glpi/glpi-agent_1.4-1_all.deb
 if test -f "$FILE"; then
     echo -e "\n$FILE exists.\n"
@@ -7,9 +6,9 @@ else
     cd /root/glpi && wget https://github.com/glpi-project/glpi-agent/releases/download/1.4/glpi-agent_1.4-1_all.deb
 fi
 chmod 777 /root/glpi/glpi-agent_1.4-1_all.deb
-sudo apt install -y /root/glpi/glpi-agent_1.4-1_all.deb
-sudo glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon
+apt install -y /root/glpi/glpi-agent_1.4-1_all.deb
+glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon
 
 command="glpi-agent --server https://inventory.globalsys.com.br/glpi --daemon"
 job="0 15 * * * $command"
-cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | sudo crontab - -u root
+cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab - -u root
